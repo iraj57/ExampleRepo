36c2a692c98f71dc4713114dabe98ae8f9ef8429
iraj57 <125519178+iraj57@users.noreply.github.com>
2023-02-17 22:04:53 -0600

    Update PracticeOne.java
    remote repo commit

--- src/practice/PracticeOne.java
+++ src/practice/PracticeOne.java
@@ -5,6 +5,9 @@ public class PracticeOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+	public void remoteMethod() {
+		// this is from remote repo
 	}
 
 }
